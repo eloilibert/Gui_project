d82611f2e3083103ae230d47128bda2dfa14f952
eloilibert <eloilibert3@gmail.com>
2026-03-24 16:04:19 +0300

    UI Architecture & Layout

--- GuiProject/ViewModels/MainViewModel.cs
+++ GuiProject/ViewModels/MainViewModel.cs
@@ -1,7 +1,43 @@
 ﻿
+
+
+//public class MainViewModel : ViewModelBase
+//{
+//    public string Greeting { get; } = "Yollaaa!";
+//}
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace GuiProject.ViewModels;
 
-public class MainViewModel : ViewModelBase
+public class MainViewModel : INotifyPropertyChanged
 {
-    public string Greeting { get; } = "Yollaaa!";
+    private string _telemetryData = "No data yet...";
+    public string TelemetryData
+    {
+        get => _telemetryData;
+        set
+        {
+            _telemetryData = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string _status = "Disconnected";
+    public string Status
+    {
+        get => _status;
+        set
+        {
+            _status = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged([CallerMemberName] string name = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
 }
--- GuiProject/Views/MainView.axaml.cs
+++ GuiProject/Views/MainView.axaml.cs
@@ -1,11 +1,12 @@
 using Avalonia.Controls;
+using GuiProject.ViewModels;
 
 namespace GuiProject.Views;
-
 public partial class MainView : UserControl
 {
     public MainView()
     {
         InitializeComponent();
+        DataContext = new MainViewModel();
     }
 }
