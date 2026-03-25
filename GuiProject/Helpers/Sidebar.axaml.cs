c31cc407304fbe8624b361cd4fa198b6e3346988
estherpolie <polieesther6@gmail.com>
2026-03-25 17:06:25 +0300

    added icons

---
+++ GuiProject/Helpers/Sidebar.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace GuiProject;
+
+public partial class Sidebar : UserControl
+{
+    public Sidebar()
+    {
+        InitializeComponent();
+    }
+}
